4f204b742fa6489bb7683df2cff7f2751d4ad0d7
Juvaria Shahid <juvariashahid@gmail.com>
2018-01-17 11:23:40 -0500

    Made the required changes

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('index.html')
+@app.route('/js5160', methods=['GET'])
+def main()
+    return render_template('adilabs.html')
 
 
 if __name__ == '__main__':
     app.run()
+
